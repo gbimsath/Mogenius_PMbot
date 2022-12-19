efbbac04f5d2f83a0b91be73ef965bacde027878
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2022-12-19 22:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone 
+https://gbimsath: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@github.com/gbimsath/pm-botx ok && cd ok && pip3 install -U -r requirements.txt && python3 -m bot &")
